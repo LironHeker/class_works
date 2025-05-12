151374c3be1f26466389a5c6ba6de7cd5666bfe1
LironHeker <liron.heker1@gmail.com>
2025-05-12 12:38:48 +0300

    Add files via upload

---
+++ tar3_class.js
@@ -0,0 +1,51 @@
+ 'use strict';
+
+ //task 5
+
+// const fruits = ["apple", "banana", "cherry", "date"];
+// fruits.splice(1,1,"blueberry",'blackberry')
+// console.log(fruits)
+
+
+//task#6
+
+// const colors = ["red", "orange", "yellow", "green", "blue"];
+// const result = colors.slice(1,3)
+// console.log(result)
+
+
+//task7
+
+// const animals1 = ["cat", "dog"];
+// const animals2 = ["lion", "tiger"];
+// const animalsall = animals1.concat(animals2)
+// console.log(animalsall)
+
+
+//task8
+
+// const numbers = [7, 3, 9, 1, 5];
+// console.log(numbers.sort())
+
+
+//task9
+
+// const letters = ["a", "b", "c", "d"];
+// letters.reverse()
+// console.log(letters)
+
+
+//task10
+
+
+// const sentence = "JavaScript is awesome";
+// const arr = sentence.split("")
+// arr.splice(0,22,"JavaScript", "is", "awesome")
+// console.log(arr)
+
+
+//task11
+
+// const words = ["Hello", "world"];
+// const str = words.join(" ")
+// console.log(str)
