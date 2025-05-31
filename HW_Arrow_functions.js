65d9dd944e40f1da0544a029d364daf403b79ab3
LironHeker <liron.heker1@gmail.com>
2025-05-31 13:05:10 +0300

    Add files via upload

---
+++ HW_Arrow_functions.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//task 1 
+// const addExclamation = str => str + '!';
+// console.log(addExclamation('hi'))// Output: "hi!"
+
+//task 2
+// const reverseWords = sentence => sentence.split(' ').reverse().join(' ');
+// console.log(reverseWords("I love JS")); // Output: "JS love I"
+
+//task 3
+// const squareAll = arr => arr.map(num => num * num);
+// console.log(squareAll([1,2,3])); // Output (1, 4, 9)
+
+//task4
+// const filterNames = (names, minLength) => names.filter(name => name.length > minLength);
+// console.log(filterNames(["Dan", "Jonathan", "Eli"], 3)); 
+// Output: ["Jonathan", "Eli"]
+
+//task 5
+// const allPositive = numbers => numbers.every(num => num > 0);
+// console.log(allPositive([1, 5, 8]));     // true
+// console.log(allPositive([3, -2, 0]));    // false
