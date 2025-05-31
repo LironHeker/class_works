4854f5ea6818a16dda12b6b649c3d0333bf63d33
LironHeker <liron.heker1@gmail.com>
2025-05-31 17:54:28 +0300

    Add files via upload

---
+++ אירועי דפדפן.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//task1
+// const colorin=document.getElementById('hoverBox')
+// colorin.addEventListener('mouseover', function () {
+//    colorin.style.backgroundColor = 'blue';
+//  });
+
+//  //task2
+//  const colorout=colorin
+//  colorout.addEventListener('mouseout', function () {
+//     colorout.style.backgroundColor = 'grey';
+//  })
+
+ //task3
+//   let count = 0; // משתנה שמחזיק את מספר הלחיצות
+
+//   const button = document.getElementById('clickBtn'); // מוצא את הכפתור
+//   const counterDisplay = document.getElementById('clickCount'); // מוצא את המקום שבו מוצג המספר
+
+//   button.addEventListener('click', function() {
+//     count++; // מוסיף 1 למספר הלחיצות
+//     counterDisplay.textContent = count; // מעדכן את הטקסט בדף
+//   });
